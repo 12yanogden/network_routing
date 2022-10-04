99ece847e1fbcb9e9ccd3a60ef442edc19b98939
12yanogden <12yanogden@gmail.com>
2022-10-04 12:13:13 -0600

    reformat

--- CS312Graph.py
+++ CS312Graph.py
@@ -2,48 +2,49 @@
 
 
 class CS312GraphEdge:
-    def __init__( self, src_node, dest_node, edge_length ):
-        self.src   = src_node
-        self.dest  = dest_node
-        self.length= edge_length
+    def __init__(self, src_node, end_node, edge_length):
+        self.src = src_node
+        self.end = end_node
+        self.length = edge_length
 
-    def __repr__( self ):
+    def __repr__(self):
         return self.__str__()
 
-    def __str__( self ):
-        return '(src={} dest={} length={})'.format(self.src,self.dest,self.length)
+    def __str__(self):
+        return '(src={} end={} length={})'.format(self.src, self.end, self.length)
+
 
 class CS312GraphNode:
-    def __init__( self, node_id, node_loc ):
-        self.node_id   = node_id
-        self.loc       = node_loc
-        self.neighbors = [] #node_neighbors
+    def __init__(self, node_id, node_loc):
+        self.node_id = node_id
+        self.loc = node_loc
+        self.neighbors = []  # node_neighbors
 
-    def addEdge( self, neighborNode, weight ):
-        self.neighbors.append( CS312GraphEdge(self,neighborNode,weight) )
+    # Not to be called by solver
+    def add_edge(self, neighbor_node, weight):
+        self.neighbors.append(CS312GraphEdge(self, neighbor_node, weight))
 
-    def __str__( self ):
-        neighbors = [edge.dest.node_id for edge in self.neighbors]
-        return 'Node(id:{},neighbors:{})'.format(self.node_id,neighbors)
+    def __str__(self):
+        neighbors = [edge.end.node_id for edge in self.neighbors]
+        return 'Node(id:{},neighbors:{})'.format(self.node_id, neighbors)
 
 
 class CS312Graph:
-    def __init__( self, nodeList, edgeList ):
-        self.nodes    = []
-        for i in range(len(nodeList)):
-            self.nodes.append( CS312GraphNode( i, nodeList[i] ) )
+    def __init__(self, node_list, edge_list):
+        self.nodes = []
+        for i in range(len(node_list)):
+            self.nodes.append(CS312GraphNode(i, node_list[i]))
 
-        for i in range(len(nodeList)):
-            neighbors = edgeList[i]
+        for i in range(len(node_list)):
+            neighbors = edge_list[i]
             for n in neighbors:
-                self.nodes[i].addEdge( self.nodes[n[0]], n[1] )
+                self.nodes[i].add_edge(self.nodes[n[0]], n[1])
         
-    def __str__( self ):
+    def __str__(self):
         s = []
         for n in self.nodes:
             s.append(n.neighbors)
         return str(s)
 
-    def getNodes( self ):
+    def get_nodes(self):
         return self.nodes
-
--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -6,16 +6,17 @@ import time
 
 
 class NetworkRoutingSolver:
-    def __init__( self):
+    def __init__(self):
         pass
 
-    def initializeNetwork( self, network ):
-        assert( type(network) == CS312Graph )
+    def initialize_network(self, network):
+        assert (type(network) == CS312Graph)
         self.network = network
 
-    def getShortestPath( self, destIndex ):
-        self.dest = destIndex
-        # TODO: RETURN THE SHORTEST PATH FOR destIndex
+    def get_shortest_path(self, end_index):
+        self.end = end_index
+        # Erase work from array run when doing heap run
+        # TODO: RETURN THE SHORTEST PATH FOR end_ndex
         #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
         #       IT'S JUST AN EXAMPLE OF THE FORMAT YOU'LL 
         #       NEED TO USE
@@ -25,18 +26,20 @@ class NetworkRoutingSolver:
         edges_left = 3
         while edges_left > 0:
             edge = node.neighbors[2]
-            path_edges.append( (edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)) )
+            path_edges.append((edge.src.loc, edge.end.loc, '{:.0f}'.format(edge.length)))
             total_length += edge.length
-            node = edge.dest
+            node = edge.end
             edges_left -= 1
-        return {'cost':total_length, 'path':path_edges}
+        return {'cost': total_length, 'path': path_edges}
 
-    def computeShortestPaths( self, srcIndex, use_heap=False ):
-        self.source = srcIndex
+    def compute_shortest_paths(self, src_index, use_heap=False):
+        self.source = src_index
         t1 = time.time()
         # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
         #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
         #       CALL TO getShortestPath(dest_index)
+        # Use dijkra's out of the book
+        # distance array has to be visible to getShortestPath()
+        # Create array implementation of a Q and a heap implementation. They should inherit the same interface.
         t2 = time.time()
-        return (t2-t1)
-
+        return t2 - t1
